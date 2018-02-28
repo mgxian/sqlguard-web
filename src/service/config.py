ccee52fd7aa1fe6dd2747d77b4304b134cf4fb97
will <maoguangxian@famulei.com>
2018-02-28 19:03:00 +0800

    基本骨架

---
+++ src/service/config.py
@@ -0,0 +1,45 @@
+import os
+
+base_path = os.path.abspath(os.path.dirname(__name__))
+
+
+class Config
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQL_GUARD_ADMIN = os.environ.get('SQL_GUARD_ADMIN')
+    SSL_REDIRECT = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQL_GUARD_SLOW_DB_QUERY_TIME = 0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+class DockerConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+
+config = {
+    'dev': DevConfig,
+    'prod': ProdConfig,
+    'docker': DockerConfig,
+    'default': DevConfig
+}
