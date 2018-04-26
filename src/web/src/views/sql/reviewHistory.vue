50085b6475192cec96b62ef1260539788574a91c
will <maoguangxian@famulei.com>
2018-04-26 10:50:37 +0800

    review basic finish

--- src/web/src/views/sql/reviewHistory.vue
+++ src/web/src/views/sql/reviewHistory.vue
@@ -1,32 +1,43 @@
 <template>
   <div class="table-contanier">
+    <div class="table-header">
+      <el-input class="search-input" v-model="queryText" placeholder="请输入地址" clearable></el-input>
+      <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    </div>
     <el-table class="table" v-loading.body="listLoading" element-loading-text="拼命加载中" :data="sqls">
-      <el-table-column align="center" prop="id" label="ID" sortable>
-      </el-table-column>
-      <el-table-column align="center" prop="mysql_id" label="MySQL-ID">
+      <el-table-column label="数据库">
+        <template slot-scope="scope">
+          {{scope.row.mysql.database}}
+          <el-tag v-if="filter.name === scope.row.mysql.env.name" v-for="filter in envFilters" :key="filter.name" :type="filter.type" close-transition>
+            {{scope.row.mysql.env.name_zh}}
+          </el-tag>
+        </template>
       </el-table-column>
       <el-table-column align="center" prop="sql" label="SQL">
       </el-table-column>
-      <el-table-column align="center" prop="type" label="类型">
+      <el-table-column align="center" prop="result" label="语法检查结果">
       </el-table-column>
-      <el-table-column align="center" prop="user_id" label="用户">
-      </el-table-column>
-      <el-table-column align="center" prop="status" label="状态">
-      </el-table-column>
-      <el-table-column align="center" prop="result" label="结果">
+      <el-table-column align="center" prop="result_execute" label="执行结果">
       </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
-import { getNeedReviewSqls } from '@/api/sql'
+import { getSqls } from '@/api/sql'
 
+const TYPE_INCEPTION = 1
 export default {
   data() {
     return {
       sqls: [],
-      listLoading: true
+      listLoading: true,
+      queryText: '',
+      envFilters: [
+        { name: 'Development', type: 'info' },
+        { name: 'Staging', type: 'warning' },
+        { name: 'Production', type: 'danger' }
+      ]
     }
   },
   created() {
@@ -35,10 +46,10 @@ export default {
   methods: {
     fetchSqls() {
       this.listLoading = true
-      getNeedReviewSqls().then(response => {
+      getSqls(TYPE_INCEPTION).then(response => {
         this.sqls = response
         this.listLoading = false
-        console.log(this.sqls)
+        // console.log(this.sqls)
       })
     }
   }
@@ -47,8 +58,23 @@ export default {
 
 <style lang="stylus" scoped>
 .table-contanier
-  margin 10px
+  margin 10px 100px
   border-radius 3px
   border 1px solid #ebebeb
   padding 24px
+
+  .table-header
+    .search-input
+      width 25%
+
+  .el-icon-circle-check, .el-icon-circle-close
+    font-size 24px
+    line-height 24px
+    vertical-align top
+
+  .el-icon-circle-check
+    color green
+
+  .el-icon-circle-close
+    color red
 </style>
